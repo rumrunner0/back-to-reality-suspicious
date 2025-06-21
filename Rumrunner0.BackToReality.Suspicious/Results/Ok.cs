61b13ba7f5253be33d64e10d924e285c6d7adafb
Roman Malyi <36127049+rumrunner0@users.noreply.github.com>
2025-06-21 22:54:56 +0200

    Fixed summary crefs, updated SharedExtensions

--- Rumrunner0.BackToReality.Suspicious/Results/Ok.cs
+++ Rumrunner0.BackToReality.Suspicious/Results/Ok.cs
@@ -9,6 +9,6 @@ namespace Rumrunner0.BackToReality.Suspicious.Results;
 /// It should be used to indicate that something is ok, up, has no errors or has required value.
 /// To indicate successful completion of an action, use <see cref="Success" />. <br />
 /// While it's possible to create new instances of this class,
-/// it's better to use <see cref="Suspicious.Ok" /> property of <see cref="Suspicious"/> factory.
+/// it's better to use <see cref="Monad.Suspicious.Ok" /> property of <see cref="Monad.Suspicious"/> factory.
 /// </remarks>
 public sealed record class Ok;
--- Rumrunner0.BackToReality.Suspicious/Results/Success.cs
+++ Rumrunner0.BackToReality.Suspicious/Results/Success.cs
@@ -9,6 +9,6 @@ namespace Rumrunner0.BackToReality.Suspicious.Results;
 /// It should be used to indicate successful completion of an operation.
 /// To indicate that something is ok, up, has no errors or has required value, use <see cref="Ok" />. <br />
 /// While it's possible to create new instances of this class,
-/// it's better to use <see cref="Suspicious.Success" /> property of <see cref="Suspicious"/> factory.
+/// it's better to use <see cref="Monad.Suspicious.Success" /> property of <see cref="Monad.Suspicious"/> factory.
 /// </remarks>
 public sealed record class Success;
