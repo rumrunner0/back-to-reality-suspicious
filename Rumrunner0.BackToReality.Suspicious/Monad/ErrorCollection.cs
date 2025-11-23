e7ae270e19e0d6a8809c91c398413f845e12d58a
Roman Malyi <36127049+rumrunner0@users.noreply.github.com>
2025-11-23 20:08:06 +0100

    Updated Shared

--- Rumrunner0.BackToReality.Suspicious/Monad/ErrorCollection.cs
+++ Rumrunner0.BackToReality.Suspicious/Monad/ErrorCollection.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Rumrunner0.BackToReality.SharedExtensions.Exceptions;
-using Rumrunner0.BackToReality.SharedExtensions.Extensions;
+using Rumrunner0.BackToReality.SharedExtensions.Collections;
 
 namespace Rumrunner0.BackToReality.Suspicious.Monad;
 
