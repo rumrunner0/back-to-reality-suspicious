a6a1e393ecbf4019bea5911d73943051030d010b
Roman Malyi <36127049+rumrunner0@users.noreply.github.com>
2025-12-21 14:20:22 +0100

    Fixed summary for Ok result

--- Rumrunner0.BackToReality.Suspicious/Results/Ok.cs
+++ Rumrunner0.BackToReality.Suspicious/Results/Ok.cs
@@ -1,4 +1,3 @@
-using Rumrunner0.BackToReality.Suspicious.Factories;
 using Rumrunner0.BackToReality.Suspicious.Monad;
 
 namespace Rumrunner0.BackToReality.Suspicious.Results;
@@ -6,7 +5,7 @@ namespace Rumrunner0.BackToReality.Suspicious.Results;
 /// <summary>Ok result for <see cref="Suspicious{TValue}" />.</summary>
 /// <remarks>
 /// * It should be used to indicate that something is ok, up, has no errors or has required value. <br />
-/// * Access this result using <see cref="Suspicious.Ok" />. <br /><br />
+/// * Access this result using <see cref="Factories.Suspicious.Ok" />.<br /><br />
 /// * If you want to indicate successful completion of an action, use <see cref="Success" />.
 /// </remarks>
 public sealed record class Ok
--- Rumrunner0.BackToReality.Suspicious/Results/Success.cs
+++ Rumrunner0.BackToReality.Suspicious/Results/Success.cs
@@ -4,8 +4,8 @@ namespace Rumrunner0.BackToReality.Suspicious.Results;
 
 /// <summary>Success result for <see cref="Suspicious{TValue}" />.</summary>
 /// <remarks>
-/// * It should be used to indicate successful completion of an operation. <br />
-/// * Access this result using <see cref="Factories.Suspicious.Success" />. <br /><br />
+/// * It should be used to indicate successful completion of an operation.<br />
+/// * Access this result using <see cref="Factories.Suspicious.Success" />.<br /><br />
 /// * If you want to indicate that something is ok, up, has no errors or has required value, use <see cref="Ok" />.
 /// </remarks>
 public sealed record class Success
